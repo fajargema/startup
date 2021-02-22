884ab640ae94251fa93c89b5517e8a4edd57525d
fajargema <fgemar72@gmail.com>
2021-02-22 19:21:39 +0700

    login endpoint

--- handler/user.go
+++ handler/user.go
@@ -41,3 +41,32 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 
 	c.JSON(200, response)
 }
+
+func (h *userHandler) Login(c *gin.Context) {
+	var input user.LoginInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Login failed", 422, "Failed", errorMessage)
+		c.JSON(422, response)
+		return
+	}
+
+	loggedinUser, err := h.userService.Login(input)
+
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+		response := helper.APIResponse("Login failed", 400, "Failed", errorMessage)
+		c.JSON(400, response)
+		return
+	}
+
+	formatter := user.FormatUser(loggedinUser, "iniadalahtoken")
+
+	response := helper.APIResponse("Successfully loggedin", 200, "Success", formatter)
+
+	c.JSON(200, response)
+}
