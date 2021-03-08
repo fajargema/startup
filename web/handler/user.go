125548f4c05f6186e8ccb6cb60e0c1ebba3555ec
fajargema <fgemar72@gmail.com>
2021-03-08 19:26:23 +0700

    Create user

--- web/handler/user.go
+++ web/handler/user.go
@@ -22,3 +22,29 @@ func (h *userHandler) Index(c *gin.Context) {
 	}
 	c.HTML(200, "user_index.html", gin.H{"users": users})
 }
+
+func (h *userHandler) New(c *gin.Context) {
+	c.HTML(200, "user_new.html", nil)
+}
+
+func (h *userHandler) Create(c *gin.Context) {
+	var input user.FormCreateUserInput
+
+	err := c.ShouldBind(&input)
+	if err != nil {
+
+	}
+
+	registerInput := user.RegisterUserInput{}
+	registerInput.Name = input.Name
+	registerInput.Email = input.Email
+	registerInput.Occupation = input.Occupation
+	registerInput.Password = input.Password
+
+	_, err = h.userService.RegisterUser(registerInput)
+	if err != nil {
+
+	}
+
+	c.Redirect(302, "/users")
+}
